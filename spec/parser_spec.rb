a0247c6496f9f6bc327d0d6a009c78d6c2f5d709
Christian Carter <cdcarter@gmail.com>
2013-08-20 17:33:02 -0500

    a spec

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -6,4 +6,18 @@ describe Tournakit::LilyChenParser, "#parse" do
 		result = parser.parse
 		result.class.should eq(Tournakit::Game)
 	end
+
+	it "returns a game object thats useful" do
+		parser = Tournakit::LilyChenParser.new("data/Czupryn.xls")
+		result = parser.parse
+		result.teams.length.should eq 2
+		result.teams[0].should eq "Byron"
+	end
+
+	it "supports more than one round on a spreadsheet" do
+		parser = Tournakit::LilyChenParser.new("data/Czupryn.xls")
+		result = parser.parse("02")
+		result.teams.length.should eq 2
+		result.teams[0].should eq "Fenton"
+	end
 end
