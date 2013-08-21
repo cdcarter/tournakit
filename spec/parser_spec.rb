50e4484cef5ca45531dd273532b7a07bbaea2b02
Christian Carter <cdcarter@gmail.com>
2013-08-20 19:36:42 -0500

    rspec best practices

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,37 +1,35 @@
 require 'spec_helper'
 
 describe Tournakit::LilyChenParser, "#parse" do
+	let(:parser) { Tournakit::LilyChenParser.new("data/Czupryn.xls") }
+	let(:result) { parser.parse }
+
 	it "returns a game object when passed a valid spreadsheet" do
-		parser = Tournakit::LilyChenParser.new("data/Czupryn.xls")
-		result = parser.parse
-		result.class.should eq(Tournakit::Game)
+		expect(result).to be_instance_of Tournakit::Game
 	end
 
 	it "returns a game object thats useful" do
-		parser = Tournakit::LilyChenParser.new("data/Czupryn.xls")
-		result = parser.parse
-		result.teams.length.should eq 2
-		result.teams[0].should eq "Byron"
+		expect(result.teams.length).to eq 2
+		expect(result.teams[0]).to eq "Byron"
 	end
 
 	it "supports more than one round on a spreadsheet" do
-		parser = Tournakit::LilyChenParser.new("data/Czupryn.xls")
-		result = parser.parse("02")
-		result.teams.length.should eq 2
-		result.teams[0].should eq "Fenton"
+		result2 = parser.parse("02")
+		expect(result2.teams.length).to eq 2
+		expect(result2.teams[0]).to eq "Fenton"
 	end
 end
 
 describe Tournakit::LilyChenParser, "::parse_rounds" do
 	it "should parse every worksheet in the file" do
 		games = Tournakit::LilyChenParser.parse_rounds("data/Czupryn.xls")
-		games.length.should eq 10
+		expect(games.length).to eq 10
 	end
 
 	# skipped by default cause it sucks
 	it "should be able to handle the whole data set", :skip => true do
 		all_games = Dir["data/*.xls"].map {|file| Tournakit::LilyChenParser.parse_rounds(file) }
-		all_games.length.should eq 12
-		all_games[5].length.should eq 10
+		expect(all_games.length).to eq 12
+		expect(all_games[5].length).to eq 10
 	end
 end
