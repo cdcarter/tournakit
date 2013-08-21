7c03a7bdfa16ec6ce843c55f5d7a9bebce043816
Christian Carter <cdcarter@gmail.com>
2013-08-20 22:45:45 -0500

    an example script

---
+++ script/event.rb
@@ -0,0 +1,27 @@
+require 'tournakit'
+require 'pp'
+
+all_games = Dir["data/*.xls"].map {|file| Tournakit::LilyChenParser.parse_rounds(file) }
+
+all_games.flatten!
+
+round1 = all_games.group_by(&:round)[1] 
+
+tossup_stats = Array.new(20) {{tens:0,negs:0,powers:0}}
+
+round1.map do |game| 
+	game.tossups.each_with_index do |tossup, tossup_idx|
+		tossup[:buzzes].flatten.each do |buzz|
+			case buzz
+			when 10
+				tossup_stats[tossup_idx][:tens] += 1
+			when 15
+				tossup_stats[tossup_idx][:powers] += 1
+			when -5
+				tossup_stats[tossup_idx][:negs] += 1
+			end
+		end
+	end
+end
+
+pp tossup_stats
