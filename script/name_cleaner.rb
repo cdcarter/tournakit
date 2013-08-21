1de8242f15dde7a553bb6fd86df100b8b73b44de
Christian Carter <cdcarter@gmail.com>
2013-08-21 17:12:53 -0500

    an example script to show how you can normalize player names easily

---
+++ script/name_cleaner.rb
@@ -0,0 +1,20 @@
+require 'tournakit'
+require 'yaml'
+
+NTV = Tournakit::Collection.new(JSON.parse(File.read("data/NTV.json")))
+
+TEAM = "Byron"
+
+puts "Number of players on team #{TEAM}: #{NTV.players(TEAM).size}"
+puts "Loading YAML"
+names = YAML.load(File.read("script/transient.yaml"))
+names[TEAM].each {|hash|
+	hash.each {|newname, oldnames|
+		oldnames.each {|oldname|
+			NTV.rename_player(TEAM,oldname,newname)
+			puts "Replacing #{oldname} with #{newname}"
+		}
+	}
+}
+puts "Replaced names!"
+puts "Number of players on team #{TEAM}: #{NTV.players(TEAM).size}"
