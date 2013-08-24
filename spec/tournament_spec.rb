b32cc206390a0915c560f0c72db3535c29fedb35
Christian Carter <cdcarter@gmail.com>
2013-08-24 12:59:08 -0500

    refactored specs to suck less

--- spec/tournament_spec.rb
+++ spec/tournament_spec.rb
@@ -20,117 +20,111 @@ describe Tournakit::Tournament do
 		end
 	end
 
-	describe "standings methods" do
+	describe Tournakit::Tournament::Team do
 		describe "#wins" do
 			it "reflects how many games the given team has won" do
-				expect(event.wins("Chicago A")).to eq(3)
-				expect(event.wins("Harvard A")).to eq(0)
+				expect(event.team("Chicago A").wins).to eq(3)
+				expect(event.team("Harvard A").wins).to eq(0)
 			end
 		end
 		describe "#losses" do
 			it "reflects how many games the given team has lost" do
-				expect(event.losses("Chicago A")).to eq(0)
-				expect(event.losses("Minnesota A")).to eq(2)
+				expect(event.team("Chicago A").losses).to eq(0)
+				expect(event.team("Minnesota A").losses).to eq(2)
 			end
 		end
 		describe "#ties" do
 			it "reflects how many ties there are" do
 				pending "totally doesn't actually do anything"
-				expect(event.ties("Chicago A")).to eq(0)
+				expect(event.team("Chicago A").ties).to eq(0)
 			end
 		end
 		describe "#pct" do
 			it "shows a win percentage" do
-				expect(event.pct("Chicago A")).to be_within(0.01).of(1.0)
-				expect(event.pct("Minnesota A")).to be_within(0.01).of(0.33)
+				expect(event.team("Chicago A").pct).to be_within(0.01).of(1.0)
+				expect(event.team("Minnesota A").pct).to be_within(0.01).of(0.33)
 			end
 		end
-	end
 
-	describe "tossup methods" do
 		describe "#tens" do
 			it "returns the number of tossups the team got 10 pts on" do
-				expect(event.tens "Chicago A").to eq 28
-				expect(event.tens "Harvard A").to eq 18
+				expect(event.team("Chicago A").tens).to eq 28
+				expect(event.team("Harvard A").tens).to eq 18
 			end
 		end
 
 		describe "#powers" do
 			it "returns the number of powers the team got 10 pts on" do
-				expect(event.powers "Chicago A").to eq 7
-				expect(event.powers "Minnesota A").to eq 7
+				expect(event.team("Chicago A").powers).to eq 7
+				expect(event.team("Minnesota A").powers).to eq 7
 			end
 		end
 
 		describe "#interrupts" do
 			it "returns the number of interrupts the team was penalized on" do
-				expect(event.interrupts "Illinois A").to eq 6
-				expect(event.interrupts "Harvard A").to eq 8
+				expect(event.team("Illinois A").interrupts).to eq 6
+				expect(event.team("Harvard A").interrupts).to eq 8
 			end
 		end
 
 		describe "#tossups_heard" do
 			it "returns the number of tossups that team heard during the tournament" do
-				expect(event.tossups_heard "Illinois A").to eq 60
+				expect(event.team("Illinois A").tossups_heard).to eq 60
 				pending "depends on round#tossups to exist, because right now TUH isn't being stored for a round."
 			end
 		end
 
 		describe "#pptuh" do
 			it "returns the points per tossup heard" do
-				expect(event.pptuh "Illinois A").to be_within(0.1).of(16.3)
+				expect(event.team("Illinois A").pptuh).to be_within(0.1).of(16.3)
 			end
 		end
 
 		describe "#ppi" do
 			it "returns the powers per interrupts" do
-				expect(event.ppi "Illinois A").to be_within(0.1).of(1.67)
+				expect(event.team("Illinois A").ppi).to be_within(0.1).of(1.67)
 			end
 		end
 
 		describe "#gpi" do
 			it "returns the gets (tens+powers) per interrupt" do
-				expect(event.gpi "Minnesota A").to be_within(0.1).of(4)
+				expect(event.team("Minnesota A").gpi).to be_within(0.1).of(4)
 			end
 		end
-	end
 
-	describe "bonus methods" do
 		describe "#bonus_points" do
 			it "should contain the total bpts" do
-				expect(event.bonus_points "Chicago A").to eq 770
+				expect(event.team("Chicago A").bonus_points).to eq 770
 			end
 		end
 
 		describe "#bonuses_heard" do
 			it "should contain the # of bonuses heard" do
-				expect(event.bonuses_heard "Chicago A").to eq 35
+				expect(event.team("Chicago A").bonuses_heard).to eq 35
 			end
 		end
 
 		describe "#ppb" do
 			it "should contain the bonus conversion" do
-				expect(event.ppb "Chicago A").to be_within(0.1).of(22.0)
+				expect(event.team("Chicago A").ppb).to be_within(0.1).of(22.0)
 			end
 		end
-	end
 
-	describe "game point methods" do
 		describe "#ppg" do
 			it "calculates ppg" do
-				expect(event.ppg "Illinois A").to be_within(0.1).of(326.7)
+				expect(event.team("Illinois A").ppg).to be_within(0.1).of(326.7)
 			end
 		end
 
 		describe "#papg" do
 			it "calculates papg" do
-				expect(event.papg "Illinois A").to be_within(0.1).of(293.3)
+				expect(event.team("Illinois A").papg).to be_within(0.1).of(293.3)
 			end
 		end
 
 		describe "#mrg" do
 			it "calculates win margin" do
-				expect(event.mrg "Illinois A").to be_within(0.1).of(33.3)
+				expect(event.team("Illinois A").mrg).to be_within(0.1).of(33.3)
 			end
 		end
 	end
