1f3cf42876d3400ba8f57b1a9f473703fcf7a5d8
Christian Carter <cdcarter@gmail.com>
2013-08-24 12:39:19 -0500

    clarifying tht output should go in output/

--- script/sqbs_out.rb
+++ script/sqbs_out.rb
@@ -3,4 +3,4 @@ require 'tournakit'
 EX = Dir["data/EXAMPLE/*.xls"].map {|f| Tournakit::LilyChenParser.parse_rounds(f)}.flatten
 Tournament = Tournakit::Collection.new(EX)
 writer = Tournakit::SQBSWriter.new(Tournament)
-File.open("output.qzx","w") {|f| f<< writer.sqbs}
+File.open("output/output.qzx","w") {|f| f<< writer.sqbs}
