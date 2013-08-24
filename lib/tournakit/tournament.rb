28c3f5cf66bae6edb6d349bb808dcace3e73bdce
Christian Carter <cdcarter@gmail.com>
2013-08-23 22:14:31 -0500

    the basicest of functionality is happening

--- lib/tournakit/tournament.rb
+++ lib/tournakit/tournament.rb
@@ -1,5 +1,6 @@
 module Tournakit
-	# +Tournament+s are special +Collection+s where every +Game+ is from the same event. In a +Tournament+, statistics like standing and ppg are meaningful. 
+	# +Tournament+s are special +Collection+s where every +Game+ is from the same event. 
+	# In a +Tournament+, statistics like standing and ppg are meaningful. 
 	class Tournament < Collection
 
 		# Team is only useful in the context of a +Tournament+
@@ -9,7 +10,7 @@ module Tournakit
 		# the +Tournament+ version of teams provides statistics and +team_id+s
 		# @return [Array<Team>]
 		def teams
-			@rounds.map(&:teams).flatten.uniq.sort.map do |team|
+			@teams ||= @rounds.map(&:teams).flatten.uniq.sort.map do |team|
 				t = Team.new
 				t.name = team
 				# these are the accumulator variables, the rest of the stats will be calculated
@@ -25,16 +26,20 @@ module Tournakit
 				t.bonuses_heard = 0
 				t.bonus_points = 0
 				@rounds.each do |round|
-					tid = round.teams.index(team)
-					
+					# make sure the team is in the round, and if it is, put its index in +tid+
+					if tid = round.teams.index(team)
+						otid = [true,false][tid] ? 1 : 0
+						round.score[tid] > round.score[otid] ? t.wins +=1 : t.losses += 1
+					end
 				end
 
 				t
 			end
+			return @teams
 		end
 
 		def wins(team)
-			@team
+			teams.find{|t|t.name==team}.wins
 		end
 	end
 end
