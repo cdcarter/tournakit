196d75923dcd9855be8cc62ff1b7d571da155843
Christian Carter <cdcarter@gmail.com>
2013-08-23 23:10:22 -0500

    failing specs for tossup stats

--- spec/tournament_spec.rb
+++ spec/tournament_spec.rb
@@ -10,25 +10,25 @@ describe Tournakit::Tournament do
 	end
 
 	describe "standings methods" do
-		describe do
+		describe "#wins" do
 			it "reflects how many games the given team has won" do
 				expect(event.wins("Chicago A")).to eq(3)
 				expect(event.wins("Harvard A")).to eq(0)
 			end
 		end
-		describe do
+		describe "#losses" do
 			it "reflects how many games the given team has lost" do
 				expect(event.losses("Chicago A")).to eq(0)
 				expect(event.losses("Minnesota A")).to eq(2)
 			end
 		end
-		describe do
+		describe "#ties" do
 			it "reflects how many ties there are" do
 				pending "totally doesn't actually do anything"
 				expect(event.ties("Chicago A")).to eq(0)
 			end
 		end
-		describe do
+		describe "#pct" do
 			it "shows a win percentage" do
 				expect(event.pct("Chicago A")).to be_within(0.01).of(1.0)
 				expect(event.pct("Minnesota A")).to be_within(0.01).of(0.33)
@@ -36,8 +36,33 @@ describe Tournakit::Tournament do
 		end
 	end
 
-	describe do
-		
+	describe "tossup methods" do
+		describe "#tens" do
+			it "returns the number of tossups the team got 10 pts on" do
+				expect(event.tens "Chicago A").to eq 28
+				expect(event.tens "Harvard A").to eq 18
+			end
+		end
+
+		describe "#powers" do
+			it "returns the number of powers the team got 10 pts on" do
+				expect(event.powers "Chicago A").to eq 7
+				expect(event.powers "Minnesota A").to eq 7
+			end
+		end
+
+		describe "#interrupts" do
+			it "returns the number of interrupts the team was penalized on" do
+				expect(event.interrupts "Illinois A").to eq 6
+				expect(event.interrups "Harvard A").to eq 8
+			end
+		end
+
+		describe "#tossups_heard" do
+			it "returns the number of tossups that team heard during the tournament" do
+				expect(event.tuh "Illinois A").to eq 60
+			end
+		end
 	end
 
 	describe "#teams" do
